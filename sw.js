5287b2e749e77ca2322121d469696dcb2b486b2d
sharmaamitskr-collab <sharmaamitskr@gmail.com>
2026-03-03 01:05:27 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,23 @@
-const CACHE = 'aapkidhun-v6';
+const CACHE = 'aapkidhun-v7';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.json', './icon.svg'];
-
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null))
-    )
-  );
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null))
+  ));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
   e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        if (res && res.status === 200) {
-          const clone = res.clone();
-          caches.open(CACHE).then(c => c.put(e.request, clone));
-        }
-        return res;
-      })
-      .catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
+    fetch(e.request).then(res => {
+      if (res && res.status === 200) {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }).catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
   );
 });
